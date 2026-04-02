1962887651abb62e4b3cc03ba9d17353490a349e
cord3690kyle <campney.cordy@gmail.com>
2026-04-02 17:18:24 -0400

    Update supabaseClient.js

--- backend/src/supabaseClient.js
+++ backend/src/supabaseClient.js
@@ -1,1 +1,8 @@
+const { createClient } = require('@supabase/supabase-js')
 
+const supabaseUrl = 'YOUR_SUPABASE_URL'
+const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+module.exports = { supabase }
